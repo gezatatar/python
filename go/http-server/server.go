4afa92f7147db23175dcb2094c14bc1de81f7de8
Géza Tatár <gezatatar@googlemail.com>
2024-03-16 18:28:39 +0100

    turn it to a simple http server

--- go/http-server/server.go
+++ go/http-server/server.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("This is my website!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Hello World!\n")
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	//fmt.Println("Hello world!")
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+	http.ListenAndServe(":8080", nil)
 }
